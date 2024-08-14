f4365eea6c7a66c807957265cba26f375413736c
Muh Aziz Iqbal <muhaziziqbal06@gmail.com>
2024-08-14 12:04:57 +0700

    feat: add validation for the .results

--- src/api/fetch/contact.ts
+++ src/api/fetch/contact.ts
@@ -1,10 +1,17 @@
 import type { ResultContact } from "@/types/Contact";
 
+function isResultContactArray(data: ResultContact) {
+  return Array.isArray(data);
+}
+
 export const fetchContact = async (): Promise<ResultContact[]> => {
   const response = await fetch("http://localhost:5000/contact");
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
   const data = await response.json();
+  if (!data.results || !isResultContactArray(data.results)) {
+    throw new Error("Invalid data format");
+  }
   return data.results;
 };
--- src/api/fetch/stages.ts
+++ src/api/fetch/stages.ts
@@ -1,10 +1,16 @@
 import type { ResultStages } from "@/types/Stages";
 
+function isResultStagesArray(data: ResultStages) {
+  return Array.isArray(data);
+}
 export const fetchStages = async (): Promise<ResultStages[]> => {
   const response = await fetch("http://localhost:5000/stages");
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
   const data = await response.json();
+  if (!data.results || !isResultStagesArray(data.results)) {
+    throw new Error("Invalid data format");
+  }
   return data.results;
 };
